8c153f157047d30b7b75fd36aeff0f3e48947987
wjrboxbbll <jiarui.wang523@gmail.com>
2016-05-15 14:01:06 -0700

    Initial Commit

---
+++ MatchismoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MatchismoDemo
+//
+//  Created by Jerry on 5/15/16.
+//  Copyright © 2016 Jerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MatchismoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MatchismoDemo
+//
+//  Created by Jerry on 5/15/16.
+//  Copyright © 2016 Jerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
